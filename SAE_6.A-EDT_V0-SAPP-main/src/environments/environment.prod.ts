5989280a4115d3777466dec77c857d49a3cca64a
cgrosjean <101260907+CyrilGrj@users.noreply.github.com>
2024-05-24 15:17:34 +0200

    fix url api

--- SAE_6.A-EDT_V0-SAPP-main/src/environments/environment.prod.ts
+++ SAE_6.A-EDT_V0-SAPP-main/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: true,
-    apiUrl: "http://srvinfo.iut.univ-paris8.fr:5000",
+    // apiUrl: "http://srvinfo.iut.univ-paris8.fr:5000",
+    apiUrl: "http://localhost:5000",
 
   };
   
--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [[ "$OS" == "Windows_NT" ]]; then
-  echo "Le script s'exécute sous Windows."
-else
-  echo "Le script s'exécute sous un système d'exploitation autre que Windows."
-fi
