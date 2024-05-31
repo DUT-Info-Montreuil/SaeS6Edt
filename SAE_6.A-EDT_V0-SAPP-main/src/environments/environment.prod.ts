03522d0875c948a291c4f169b90bad898de4106d
cgrosjean <101260907+CyrilGrj@users.noreply.github.com>
2024-05-31 11:09:00 +0200

    change url api

--- SAE_6.A-EDT_V0-SAPP-main/src/environments/environment.prod.ts
+++ SAE_6.A-EDT_V0-SAPP-main/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    // apiUrl: "http://srvinfo.iut.univ-paris8.fr:5000",
-    apiUrl: "http://localhost:8100",
+    apiUrl: "http://srvinfo.iut.univ-paris8.fr:5000",
+    // apiUrl: "http://localhost:8100",
 
   };
   
