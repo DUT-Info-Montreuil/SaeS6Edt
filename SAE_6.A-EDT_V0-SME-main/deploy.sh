3c50800ae04419dd6e9a45ac79ea361b7861a5a1
Gilgamesh-lab <fabrice.amegadjen@gmail.com>
2024-05-31 16:31:26 +0200

    Correction du bug d'affichage quand on se connecte avec le compte de Mme Ricordeau

--- SAE_6.A-EDT_V0-SME-main/deploy.sh
+++ SAE_6.A-EDT_V0-SME-main/deploy.sh
@@ -8,7 +8,6 @@ docker stop courroux-sme
 docker rm --force courroux-sme
 # docker images | grep none | awk '{ print $3; }' | xargs docker rmi --force
 echo '2/7 Docker build'
-# docker build --pull --rm -f "Dockerfile" -t courroux-sme:0.0.1 "."
 docker build -t courroux-sme:0.0.1 .
 echo '3/7 Docker run'
 docker run -d --name courroux-sme courroux-sme:0.0.1
@@ -17,7 +16,6 @@ docker commit courroux-sme courroux-sme:0.0.1
 # echo '5/7 Docker save'
 # docker save courroux-sme:0.0.1 > dist/courroux-sme.tar
 # echo '6/7 Docker remove old images'
-# docker images | grep none | awk '{ print $3; }' | xargs docker rmi --force
 echo '6/7 Docker stop new service and remove it'
 docker stop courroux-sme
 docker rm courroux-sme
