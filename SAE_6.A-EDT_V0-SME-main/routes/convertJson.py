a8ad5c9ce28c1fa63b069284c080ac27c5dd3d1e
Bastien <bastien.pelletier01@gmail.com>
2024-06-06 10:17:11 +0200

    init

---
+++ SAE_6.A-EDT_V0-SME-main/routes/convertJson.py
@@ -0,0 +1,86 @@
+
+
+
+
+"""
+converFlopToEdt(json):
+    
+    
+    
+    
+    
+    
+
+ {
+        "id": 1041,
+        "room": {
+            "id": 186,
+            "name": "Amphi2"
+        },
+        "start_time": 670,
+        "day": "tu",
+        "course": {
+            "id": 1019,
+            "type": "CM90",
+            "room_type": "CoursInfo",
+            "week": 14,
+            "year": 2024,
+            "groups": [
+                {
+                    "id": 23,
+                    "train_prog": "BUT1",
+                    "name": "S2Cours",
+                    "is_structural": true
+                }
+            ],
+            "supp_tutor": [],
+            "module": {
+                "name": "Interactions Hommes Machines",
+                "abbrev": "R202",
+                "display": {
+                    "color_bg": "#8fba06",
+                    "color_txt": "#000000"
+                }
+            },
+            "pay_module": null,
+            "is_graded": false
+        },
+        "tutor": "MS",
+        "id_visio": null,
+        "number": 1
+    },
+
+
+
+
+
+    {
+        "appelEffectue": false,
+        "end_time": "2024-05-30 16:06:00",
+        "evaluation": false,
+        "id": 21,
+        "id_enseignant": 1,
+        "id_group": 1,
+        "initial_ressource": "R3-01",
+        "is_published": 0,
+        "name_salle": "A2-01",
+        "start_time": "2024-05-30 14:04:00"
+    }
+
+    {
+        "appelEffectue": false,
+        "end_time": "2024-05-31 15:09:00",
+        "evaluation": false,
+        "id": 11,
+        "id_enseignant": 1,
+        "id_group": 1,
+        "initial_ressource": "R3-02",
+        "is_published": 0,
+        "name_salle": "A2-03",
+        "start_time": "2024-05-31 14:08:00"
+    }
+
+}
+"""
+
+
