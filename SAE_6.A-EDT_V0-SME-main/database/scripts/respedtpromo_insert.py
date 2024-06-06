87d8041f23c44a6af50da1726e338136ccce7d12
Jonas Obrun <jonas.obrun@gmail.com>
2024-06-06 14:53:57 +0200

    init branch test

--- SAE_6.A-EDT_V0-SME-main/database/scripts/respedtpromo_insert.py
+++ SAE_6.A-EDT_V0-SME-main/database/scripts/respedtpromo_insert.py
@@ -18,6 +18,36 @@ dataRespEDT = [
   }
 ]
 
+# Département INFO
+# p.bonnot@iut.univ-paris8.fr
+# n.mellouli@iut.univ-paris8.fr
+# m.simonot@iut.univ-paris8.fr
+# m.homps@iut.univ-paris8.fr
+# m.cataldi@iut.univ-paris8.fr
+# a.bossard@iut.univ-paris8.fr
+
+# Département GACO
+# Amine ZIZI <a.zizi@iut.univ-paris8.fr>
+# Vincent LEBEL <v.lebel@iut.univ-paris8.fr>
+# Stéphane HURTADO <s.hurtado@iut.univ-paris8.fr>
+# DHENIN Jean-François <jf.dhenin@iut.univ-paris8.fr>
+# BABOULALL Marielle <m.baboulall@iut.univ-paris8.fr>
+# Noiric SAINTINI <n.saintini@iut.univ-paris8.fr>
+# Catherine DURAND <c.durand@iut.univ-paris8.fr>
+
+
+# Département QLIO
+# s.menjeli@iut.univ-paris8.fr
+# c.ballaydally@iut.univ-paris8.fr
+# y.hani@iut.univ-paris8.fr
+
+# Département INFO-COM
+# m.kaiser@iut.univ-paris8.fr
+# vivianeclaus@icloud.com
+
+
+
+
 
 for resp in dataRespEDT:
     ResponsableEdtService.create_responsable_edt(resp)
