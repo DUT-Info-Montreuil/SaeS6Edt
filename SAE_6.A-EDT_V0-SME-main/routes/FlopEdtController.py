56134e318d753605d41f5cd2a11d37c4602d7179
bseydi <boulaye.seydi@gmail.com>
2024-06-13 00:18:25 +0200

    bug fix and add a requirement

--- SAE_6.A-EDT_V0-SME-main/routes/FlopEdtController.py
+++ SAE_6.A-EDT_V0-SME-main/routes/FlopEdtController.py
@@ -1,15 +1,23 @@
 from flask import Blueprint, jsonify, request
 from flask_jwt_extended import jwt_required
-from services.flop_edt_service import FlopEdtServices
+from services.FlopEdtService import FlopEdtService
 
 flopEdt_bp = Blueprint('flopEdt', __name__)
 
-@flopEdt_bp.route('/flopEdt/<dept>/<week>/<year>/<work_copy>', methods=['GET'])
+@flopEdt_bp.route('/flopEdt', methods=['POST'])
 @jwt_required()
-def get_FlopEdt_courses(dept, week, year, work_copy):
-    data = FlopEdtService.get_FlopEdt(dept, week, year, work_copy)
+def get_FlopEdt_courses():
+    # Récupérer les paramètres du corps de la requête
+    data = request.get_json()
+    dept = data.get('dept')
+    week = data.get('week')
+    year = data.get('year')
+    work_copy = data.get('work_copy')
     
-    if data is None:
-        return jsonify({"error": "An error occurred while fetching the data"}), 500
+    # Appel au service pour obtenir les données
+    response_data = FlopEdtService.get_FlopEdt_courses(dept, week, year, work_copy)
     
-    return jsonify(data), 200
+    # if response_data is None:
+    #     return jsonify({"error": "An error occurred while fetching the data"}), 500
+    
+    return jsonify(response_data), 200
--- SAE_6.A-EDT_V0-SME-main/services/FlopEdtService.py
+++ SAE_6.A-EDT_V0-SME-main/services/FlopEdtService.py
@@ -8,10 +8,19 @@ class FlopEdtService:
         url = f"https://flopedt.iut.univ-paris8.fr/fr/api/fetch/scheduledcourses/?dept={dept}&week={week}&year={year}&work_copy={work_copy}"
 
         try:
-            response = requests.get(url)
-            response.raise_for_status()
+            # response = requests.get(url)
+            # response.raise_for_status()
             
-            return response.json()
+            # return response.json()
+            with requests.Session() as session:
+                response = session.get(url)
+                response.raise_for_status()
+                
+                # Log the status code and content for debugging
+                print(f"Response status code: {response.status_code}")
+                print(f"Response content: {response.text}")
+                
+                return response.json()
         except requests.exceptions.RequestException as e:
             print(f"An error occurred: {e}")
             return None
