4a727738015d0ca5f1fb7196245647f848deb72e
Jonas Obrun <jonas.obrun@gmail.com>
2024-06-14 17:16:10 +0200

    Ajout de volume

--- SAE_6.A-EDT_V0-SME-main/database/scripts/__init__.py
+++ SAE_6.A-EDT_V0-SME-main/database/scripts/__init__.py
@@ -5,12 +5,12 @@ import database.scripts.teacher_insert
 import database.scripts.promotion_insert
 import database.scripts.groupe_insert
 import database.scripts.affiliationRespEdt_insert
-# import database.scripts.salle_insert
-# import database.scripts.teacher_insert
-# import database.scripts.ressources_insert
-# import database.scripts.user_groupe_insert
-# import database.scripts.cours_insert
-# import database.scripts.absence_insert
+import database.scripts.salle_insert
+import database.scripts.teacher_insert
+import database.scripts.ressources_insert
+import database.scripts.user_groupe_insert
+import database.scripts.cours_insert
+import database.scripts.absence_insert
 # import database.scripts.studentAbsencesCours_insert
 
 
