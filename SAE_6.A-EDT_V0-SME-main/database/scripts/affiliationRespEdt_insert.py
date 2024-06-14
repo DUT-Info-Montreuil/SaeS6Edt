d7d175b63b5e1dacd15124c71872f6a57e103b50
Jonas Obrun <jonas.obrun@gmail.com>
2024-06-14 18:38:45 +0200

    refactor insert

--- SAE_6.A-EDT_V0-SME-main/database/scripts/affiliationRespEdt_insert.py
+++ SAE_6.A-EDT_V0-SME-main/database/scripts/affiliationRespEdt_insert.py
@@ -19,9 +19,5 @@ for respEdt in dataAffiliationRespEdt:
 
     id_resp = respEdt["id_resp"]
     id_promo = respEdt["id_promo"]
-    existing_affiliateRespEdtAffiliate = affiliation_resp_edt.query.filter_by(id_resp=respEdt['id_resp'], id_promo=respEdt['id_promo']).first()
-    if not existing_affiliateRespEdtAffiliate:
-        AffiliationRespEdtService.affiliate_respedt_to_promo(id_resp,id_promo)
-    else:
-        print(f"Affiliation between ResponsableEdt {respEdt['id_resp']} and Promotion {respEdt['id_promo']} already exists.")
+    AffiliationRespEdtService.affiliate_respedt_to_promo(id_resp,id_promo)
 
