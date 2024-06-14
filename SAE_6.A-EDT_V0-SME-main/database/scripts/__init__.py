7c920db28642ea4aa074666bb6484945ad487fdb
Jonas Obrun <jonas.obrun@gmail.com>
2024-06-14 17:04:33 +0200

    Fixv2

--- SAE_6.A-EDT_V0-SME-main/database/scripts/__init__.py
+++ SAE_6.A-EDT_V0-SME-main/database/scripts/__init__.py
@@ -1,10 +1,10 @@
 # import database.scripts.student_insert
 import database.scripts.respedtpromo_insert
-# import database.scripts.salle_insert
-# import database.scripts.teacher_insert
-# import database.scripts.promotion_insert
-# import database.scripts.groupe_insert
-# import database.scripts.affiliationRespEdt_insert
+import database.scripts.salle_insert
+import database.scripts.teacher_insert
+import database.scripts.promotion_insert
+import database.scripts.groupe_insert
+import database.scripts.affiliationRespEdt_insert
 # import database.scripts.salle_insert
 # import database.scripts.teacher_insert
 # import database.scripts.ressources_insert
